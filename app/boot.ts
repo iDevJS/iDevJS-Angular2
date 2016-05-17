9acf3c3331de35a81180671ac926952ab0074846
xuhong <cxh710200711@gmail.com>
2016-05-18 01:46:08 +0800

    update tab header

--- app/boot.ts
+++ app/boot.ts
@@ -1,5 +1,6 @@
 import {bootstrap} from '@angular/platform-browser-dynamic'
 import {provide} from '@angular/core'
+import { Title } from '@angular/platform-browser'
 import {ROUTER_PROVIDERS} from '@angular/router'
 import 'rxjs/add/operator/map'
 import {HTTP_PROVIDERS, Http} from '@angular/http'
@@ -9,6 +10,7 @@ import {Client, AuthConfig} from 'idevjs-angular-client'
 bootstrap(AppComponent, [
     HTTP_PROVIDERS,
     ROUTER_PROVIDERS,
+    Title,
     provide(Client, {
         useFactory: (http) => {
             return new Client(new AuthConfig({
--- app/pages/home.component.ts
+++ app/pages/home.component.ts
@@ -1,5 +1,5 @@
 import {Component, ChangeDetectionStrategy, OnInit} from '@angular/core'
-import {ROUTER_DIRECTIVES} from '@angular/router'
+import {Router, RouteSegment, OnActivate, ROUTER_DIRECTIVES} from '@angular/router'
 import {AcPagination} from '../utils/pagination'
 import {NgcTabHeader} from '../utils/tab-header'
 import {PostItemComponent} from '../post/post-item.component'
@@ -14,24 +14,33 @@ import {Client} from 'idevjs-angular-client'
     // changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class HomeComponent implements OnInit {
-    public title: string = 'Let us rock'
+export class HomeComponent implements OnActivate, OnInit {
+    private curSegment: RouteSegment
     public posts: any[] = []
     public collectionSize: number = 0
     public pageSize: number = 10
     public tabList = [
+        { "alias": "全部", "name": "" },
+        { "alias": "热门", "name": "hot" },
+        { "alias": "最新", "name": "new" },
+        { "alias": "精华", "name": "recommend" },
+        { "alias": "招聘", "name": "job" },
         { "alias": "AngularJS", "name": "angular" },
         { "alias": "ReactJS", "name": "react" },
         { "alias": "VueJS", "name": "vue" }
     ]
-    constructor(private _client: Client) {
+    constructor(private _client: Client, private router: Router) {
 
+    }
+    routerOnActivate(curr: RouteSegment) {
+        this.curSegment = curr
     }
     ngOnInit() {
         this.setPage(1)
     }
     setTab(tab) {
-        console.log(tab)
+        console.log(tab, this.curSegment)
+        this.router.navigate(['/', { tab: tab.name }])
     }
     setPage(pageNo: number) {
         let startNo: number = this.pageSize * (pageNo - 1 || 0)
